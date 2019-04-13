0cf2ad209be2c26794366e1eba5d22da4b230784
Imed Adel <aadelimed@gmail.com>
2019-04-13 17:43:28 +0100

    :mag: Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `knowsby`,
+    author: `Imed Adel`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `Imed_Adel`,
     },
   },
   plugins: [
